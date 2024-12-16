4107b4a6dc2d88b702e5a96752c3370af023b9f9
sh3ll3x3c <ljubisa@ethernal.tech>
2024-12-16 10:23:20 +0100

    add CLI configuration options

---
+++ src/cli.rs
@@ -0,0 +1,26 @@
+use clap::Parser;
+use tracing::Level;
+
+#[derive(Parser, Clone, Debug)]
+pub struct CliOpts {
+    #[arg(
+        long,
+        default_value = "127.0.0.1",
+        help = "Avail Light Client Tracker listen address. Default: 127.0.0.1"
+    )]
+    pub server_addr: String,
+    #[arg(
+        long,
+        default_value = "8080",
+        help = "Avail Light Client Tracker listen port. Default: 8080"
+    )]
+    pub server_port: u16,
+    #[arg(
+        long,
+        default_value = "./ping_db",
+        help = "Avail Light Client Tracker local data location. Default: \"./ping_db\""
+    )]
+    pub db_path: String,
+    #[arg(long, default_value = "info", help = "Log verbosity. Default: info")]
+    pub verbosity: Level,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
-mod config;
+use actix_web::{web, App, HttpServer};
+use anyhow::Result;
+use clap::Parser;
+use cli::CliOpts;
+use std::sync::Arc;
+use storage::RocksStorage;
+use tracing::{info, Level, Subscriber};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+mod cli;
 mod error;
 mod handlers;
 mod storage;
 mod types;
 
-use actix_web::{web, App, HttpServer};
-use config::AppConfig;
-use std::sync::Arc;
-use storage::RocksStorage;
+pub fn default_subscriber(log_level: Level) -> impl Subscriber {
+    FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::new(format!(
+            "avail_light_tracking_service={log_level}"
+        )))
+        .finish()
+}
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let config = AppConfig::default();
-    let storage =
-        Arc::new(RocksStorage::new(&config.db_path).expect("Failed to initialize storage"));
+async fn main() -> Result<()> {
+    let opts = CliOpts::parse();
+    tracing::subscriber::set_global_default(default_subscriber(opts.verbosity))?;
+
+    info!("Starting Avail Light Tracking Service");
+    let storage = Arc::new(RocksStorage::new(opts.db_path).expect("Failed to initialize storage"));
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::from(Arc::clone(&storage)))
             .route("/ping", web::get().to(handlers::handle_ping))
     })
-    .bind(format!("{}:{}", config.server_addr, config.server_port))?
+    .bind(format!("{}:{}", opts.server_addr, opts.server_port))?
     .run()
     .await
+    .map_err(anyhow::Error::from)
 }
